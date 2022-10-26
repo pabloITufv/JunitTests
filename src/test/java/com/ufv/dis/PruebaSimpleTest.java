85ffaf74f7a6d4181a74569b25909f463723674c
“PabloC” <7736022@alumnos.ufv.es>
2022-10-26 20:24:04 +0200

    Test basico generado

---
+++ src/test/java/com/ufv/dis/PruebaSimpleTest.java
@@ -0,0 +1,12 @@
+package com.ufv.dis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PruebaSimpleTest {
+
+    @Test
+    void prueba1() {
+    }
+}
