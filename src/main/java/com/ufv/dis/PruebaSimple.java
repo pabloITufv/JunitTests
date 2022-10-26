42d0ed750a3e4f975c40392ad48190415ca8cc85
“PabloC” <7736022@alumnos.ufv.es>
2022-10-26 20:16:48 +0200

    Funcion basica creada

--- src/main/java/com/ufv/dis/PruebaSimple.java
+++ src/main/java/com/ufv/dis/PruebaSimple.java
@@ -1,4 +1,14 @@
 package com.ufv.dis;
 
 public class PruebaSimple {
+
+    //Clase que devuelve un booleano y que recibe dos parámetros
+    public boolean prueba1(int a, int b){
+        if (a > b){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
