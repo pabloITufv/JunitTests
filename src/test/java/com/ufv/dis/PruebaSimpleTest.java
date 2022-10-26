04923919d3dd38e02ce3c445d666d306d4414bed
“PabloC” <7736022@alumnos.ufv.es>
2022-10-26 20:34:15 +0200

    Prueba creada y ejecutada

--- src/test/java/com/ufv/dis/PruebaSimpleTest.java
+++ src/test/java/com/ufv/dis/PruebaSimpleTest.java
@@ -6,7 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PruebaSimpleTest {
 
+    //Hay que crear un objeto para el testeo
+    PruebaSimple miPrueba = new PruebaSimple();
     @Test
     void prueba1() {
+        assertEquals(false, miPrueba.prueba1(1,2));
     }
 }
